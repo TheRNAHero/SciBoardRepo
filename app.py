7b8ec9c3d380d49df2fc1d8258f984e013bfbd99
Gabriel Fossner <gabrielfossner@urjscitech6.org>
2022-07-05 12:10:48 -0400

    more code!

--- app.py
+++ app.py
@@ -2,20 +2,26 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 import secrets
 app=Flask(__name__)
 app.config['SECRET_KEY']=secrets.token1
-
+messages=[]
 @app.route('/')
 def backend():
-    return render_template('index.html')
-title=None
-namee=None
-content=None
+    return render_template('index.html', messages=messages)
+
+@app.route('/criminal')
+def criminal():
+    return render_template('criminal.html')
+
 @app.route('/processer', methods=["GET", "POST"])
 def iGetItNow():
     if request.method=='POST':
         title=request.form['title']
         namee=request.form['name']
         content=request.form['body']
-   
+
+    if title == "" or namee=="" or content=="":
+       pass
+    else:
+       messages.insert(0, {'title': title, 'name': namee, 'body':content})
     return redirect(url_for('backend'))
 
 if __name__ == '__main__':
